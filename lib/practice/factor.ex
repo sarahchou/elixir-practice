f42d4355730dab7ae571670c2a92a66d91669042
sarahchou <chou.s@husky.neu.edu>
2018-09-20 19:35:53 -0400

    prints factors of even numbers

--- lib/practice/factor.ex
+++ lib/practice/factor.ex
@@ -4,12 +4,29 @@ defmodule Practice.Factor do
     num
   end
 
+  #base case
+  #def factor()
+
+  # list, accumulator
+  def factor(acc, z, list) do
+    list = [z | list]
+    list = [acc | list]
+    #factor(z)
+  end
+
+  # final function
   def factor(x) do
     list = []
     list = [1 | list]
-    list = [2 | list]
-    list = [3 | list]
-    list = [x | list]
-  end
 
+    z = parse_int(x)
+    if rem(z, 2) == 0 do
+      z = div(z, 2)
+      factor(2, z, list)
+    end
+    # if rem(z, 3) == 0 do
+    #   z = div(z, 3)
+    #   factor(3, z, list)
+    # end
+  end
 end

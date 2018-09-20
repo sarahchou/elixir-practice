9472530753767b5071c82da145543e6807d366d4
sarahchou <chou.s@husky.neu.edu>
2018-09-20 19:40:10 -0400

    added cond for 3, 5, 7

--- lib/practice/factor.ex
+++ lib/practice/factor.ex
@@ -20,10 +20,27 @@ defmodule Practice.Factor do
     list = [1 | list]
 
     z = parse_int(x)
-    if rem(z, 2) == 0 do
-      z = div(z, 2)
-      factor(2, z, list)
+    cond do
+      rem(z, 2) == 0 ->
+        z = div(z, 2)
+        factor(2, z, list)
+      rem(z, 3) == 0 ->
+        z = div(z, 3)
+        factor(3, z, list)
+      rem(z, 5) == 0 ->
+        z = div(z, 5)
+        factor(5, z, list)
+      rem(z, 7) == 0 ->
+        z = div(z, 7)
+        factor(7, z, list)
+      true ->
+        list = [z | list]
     end
+
+    # if rem(z, 2) == 0 do
+    #   z = div(z, 2)
+    #   factor(2, z, list)
+    # end
     # if rem(z, 3) == 0 do
     #   z = div(z, 3)
     #   factor(3, z, list)
